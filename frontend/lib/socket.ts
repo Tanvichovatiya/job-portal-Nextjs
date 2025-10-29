e1f5230a2fef6d2e299a0b6353a54e993780174f
tanvi chovatiya <chovatiyatanvi21@gmail.com>
2025-10-29 13:49:49 +0530

    add for deploy

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_API_URL, { transports: ["websocket"] });
+export const socket = io(process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000", { transports: ["websocket"] });
 
 export function authenticateSocket(token: string) {
   if (!socket.connected) socket.connect();
