4e6213667c26ddd2b6369c176d59eb3a693db9a5
tanvi chovatiya <chovatiyatanvi21@gmail.com>
2025-10-29 13:31:10 +0530

    solve deploy error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@
 import SocketProvider from "./components/SocketProvider";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import BootstrapInit from "./components/BootstrapInit";
 export const metadata = { title: "Job Portal", description: "Real-time job portal" };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
